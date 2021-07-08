a9a337864306c77e4ad203b0bd1199af65edf032
YFK1M <laystimer@gmail.com>
2021-07-09 01:06:09 +0300

    main structure (second step)

--- index.php
+++ index.php
@@ -6,12 +6,13 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ГЭКОМ-РКС</title>
+    <link rel="stylesheet" href="css/fonts.css">
     <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
         <?php require_once ("common/nav.php"); ?>
         <main>
-            <p>Hello World</p>
+
         </main>
         <?php require_once ("common/footer.php"); ?>
 </body>
