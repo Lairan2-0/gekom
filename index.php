55f1fa6a4f5a8755e34ec3697dd94bb0513d0546
YFK1M <laystimer@gmail.com>
2021-07-09 02:18:27 +0300

    main banner additions blocks

--- index.php
+++ index.php
@@ -12,7 +12,50 @@
 <body>
         <?php require_once ("common/nav.php"); ?>
         <main>
-
+            <div class="main-banner">
+                <p class="main-banner__paragraph">
+                    Делаем вашу жизнь проще!
+                </p>
+                <h1 class="main-banner__header">
+                    Расчетно-кассовый<br>центр
+                </h1>
+            </div>
+            <div class="additions">
+                <div class="additions__block testimony">
+                    <div class="testimony__left">
+                        <h2 class="testimony___header">
+                            Передача показаний
+                        </h2>
+                        <p class="testimony__paragraph">
+                            Полная история показаний счетчиков и начислений по воде доступна в вашем личном кабинете
+                        </p>
+                    </div>
+                    <div class="testimony__right">
+                        <div class="testimony__counter">
+                            <p class="testimony__num">5</p>
+                            <p class="testimony__num">9</p>
+                            <p class="testimony__num">5</p>
+                            <p class="testimony__num">3</p>
+                            <p class="testimony__num">8</p>
+                        </div>
+                        <button class="testimony__button">Передать показания</button>
+                    </div>
+                </div>
+                <div class="additions__block online-purchase">
+                    <div class="online-purchase__left">
+                        <h2 class="online-purchase__header">
+                            Онлайн-оплата
+                        </h2>
+                        <p class="online-purchase__paragraph">
+                            Подробная информация по вашему счету или задолженности доступна в вашем личном кабинете
+                        </p>
+                    </div>
+                    <div class="online-purchase__right">
+                        <img class="online-purchase__image" src="img/phone.png">
+                        <button class="online-purchase__button">Перейти к оплате</button>
+                    </div>
+                </div>
+            </div>
         </main>
         <?php require_once ("common/footer.php"); ?>
 </body>
