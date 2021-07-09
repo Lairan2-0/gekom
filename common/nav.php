91c5f299d9b87ec1fc7a51c11335852a720ac994
YFK1M <laystimer@gmail.com>
2021-07-10 01:59:05 +0300

    nav prepare

--- common/nav.php
+++ common/nav.php
@@ -5,19 +5,19 @@
                 <img class="index-page-logo" src="/img/logo.svg" alt="main-page-logo">
             </a>
         </div>
-        <div class="contact-data-separator"></div>
         <div class="contact-data__contact-numbers">
             <p class="contact-numbers__contact-number">8 (34248) 45-914</p>
             <p class="contact-numbers__contact-number">8-909-100-10-30</p>
         </div>
-        <div class="contact-data-separator"></div>
         <div class="contact-data__address">
-            <p class="address__info">Пермский край, г. Губаха, пр-т Ленина, д. 45, оф. 4, пом. 1</p>
+            <p class="address__info">Пермский край, г. Губаха, пр-т Ленина,<br> д. 45, оф. 4, пом. 1</p>
         </div>
     </div>
     <div class="navigation-buttons">
-            <button class="navigation-buttons__index-button">Главная</button>
-            <button class="navigation-buttons__about-button">О компании</button>
-            <button class="navigation-buttons__personal-area">Личный кабинет</button>
+            <a class="navigation-buttons__index-button" href="#">Главная</a>
+            <a class="navigation-buttons__about-button" href="#">О компании</a>
+            <a class="navigation-buttons__personal-area" href="#">
+                <button>Личный кабинет</button>
+            </a>
     </div>
 </nav>
