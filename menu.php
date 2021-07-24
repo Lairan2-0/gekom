cf90db9ea86c5924f3f80a5e363890bd73a639fb
voryij <vovazoid@gmail.com>
2021-07-24 14:22:00 +0200

    mpa.php added mpa css first step

--- menu.php
+++ menu.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="rus">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
@@ -11,51 +11,47 @@
     <script src="https://cdn.jsdelivr.net/npm/simple-parallax-js@5.0.2/dist/simpleParallax.min.js"></script>
 </head>
 <body>
+        <?php require_once("common/nav.php"); ?>
+        <main>
+            <div class="menu-buttons">
+                <a href="/mpa.php" class="menu-buttons__mpa">
+                    <img class="link__image" src="/img/mpa.svg" alt="mpa-image">
+                    <p class="link__text">Мои лицевые счета</p>
+                </a>
+                <a href="" class="menu-buttons__payment">
+                    <img class="link__image" src="/img/payment.svg" alt="payment">
+                    <p class="payment__text">Оплата</p>
+                </a>
+                <a href="" class="menu-buttons__online-application">
+                    <img class="link__image" src="/img/online-application.svg" alt="online-application">
+                    <p class="online-application__text">Подать заявление онлайн</p>
+                </a>
 
-<?php require_once ("common/nav.php"); ?>
+                <a href="" class="menu-buttons__get-reference">
+                    <img class="link__image" src="/img/reference.svg" alt="reference">
+                    <p class="get-reference__text">Получить справку онлайн</p>
+                </a>
+                <img class="menu-buttons__space-between" src="/img/space-between.svg" alt="space-between">
+                <a href="" class="menu-buttons__accurals">
+                    <img class="link__image" src="/img/accurals.svg" alt="accurals">
+                    <p class="accurals__text">Начисления и платежи</p>
+                </a>
 
-<main>
-    <div class="menu-buttons">
-        <a href="" class="menu-buttons__mpa">
-            <img class="link__image" src="/img/mpa.svg" alt="mpa-image">
-            <p class="link__text">Мои лицевые счета</p>
-        </a>
-        <a href="" class="menu-buttons__payment">
-            <img class="link__image" src="/img/payment.svg" alt="payment">
-            <p class="payment__text">Оплата</p>
-        </a>
-        <a href="" class="menu-buttons__online-application">
-            <img class="link__image" src="/img/online-application.svg" alt="online-application">
-            <p class="online-application__text">Подать заявление онлайн</p>
-        </a>
-
-        <a href="" class="menu-buttons__get-reference">
-            <img class="link__image" src="/img/reference.svg" alt="reference">
-            <p class="get-reference__text">Получить справку онлайн</p>
-        </a>
-        <img class="menu-buttons__space-between" src="/img/space-between.svg" alt="space-between">
-        <a href="" class="menu-buttons__accurals">
-            <img class="link__image" src="/img/accurals.svg" alt="accurals">
-            <p class="accurals__text">Начисления и платежи</p>
-        </a>
-
-        <a href="" class="menu-buttons__indications">
-            <img class="link__image" src="/img/indications.svg" alt="indications">
-            <p class="indications__text">Показания</p>
-        </a>
-        <a href="" class="menu-buttons__question">
-            <img class="link__image" src="/img/question.svg" alt="question">
-            <p class="question__text">Задать вопрос</p>
-        </a>
-        <a href="" class="menu-buttons__main">
-            <img class="link__image" src="/img/back-arrow.svg" alt="back-arrow">
-            <p class="main__text">Вернуться на главную</p>
-        </a>
-    </div>
-</main>
-
-<?php require_once ("common/footer.php"); ?>
-<script src="js/main.js"></script>
-
+                <a href="" class="menu-buttons__indications">
+                    <img class="link__image" src="/img/indications.svg" alt="indications">
+                    <p class="indications__text">Показания</p>
+                </a>
+                <a href="" class="menu-buttons__question">
+                    <img class="link__image" src="/img/question.svg" alt="question">
+                    <p class="question__text">Задать вопрос</p>
+                </a>
+                <a href="" class="menu-buttons__main">
+                    <img class="link__image" src="/img/back-arrow.svg" alt="back-arrow">
+                    <p class="main__text">Вернуться на главную</p>
+                </a>
+            </div>
+        </main>
+        <?php require_once("common/footer.php"); ?>
+        <script src="js/main.js"></script>
 </body>
 </html>
---
+++ mpa.php
@@ -0,0 +1,98 @@
+<!doctype html>
+<html lang="rus">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ГЭКОМ-РКС-ЛИЧНЫЙ-КАБИНЕТ</title>
+    <link rel="stylesheet" href="css/fonts.css">
+    <link rel="stylesheet" href="css/main.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<?php require_once("common/nav.php"); ?>
+
+<main>
+    <hr class="mpa-hr">
+    <div class="upper-info">
+        <div class="upper-info__left">
+            <div class="left__left-info">
+                <div class="left-info__full-name">
+                    <h3 class="full-name__indication">ФИО:</h3>
+                    <p class="full-name__fillable">Орлов Василий Юрьевич</p>
+                </div>
+                <div class="left-info__accordance">
+                    <h3 class="accordance__indications">ДОГОВОР ЖКХ:</h3>
+                    <p class="accordance__fillable">1149410082</p>
+                </div>
+            </div>
+            <div class="left__right-info">
+                <div class="right-info__address">
+                    <h3 class="address__indications">АДРЕС:</h3>
+                    <p class="address__fillable">618254, Пермский край, г.Губаха, пр-кт.<br> Свердлова, д.6, корп. В, кв.44</p>
+                </div>
+                <div class="right-info__telephone">
+                    <h3 class="telephone__indications">ТЕЛЕФОН:</h3>
+                    <p class="telephone__fillable">8(902)-633-20-35</p>
+                </div>
+            </div>
+        </div>
+        <div class="upper-info__right">
+            <div class="right__count">
+                <h3 class="count__indications">ЛИЦЕВОЙ СЧЁТ:</h3>
+                <p class="count__fillable">1149410082</p>
+            </div>
+            <div class="right__to-pay">
+                <h3 class="to-pay__indications">К ОПЛАТЕ:</h3>
+                <p class="to-pay__fillable">623.12 р.</p>
+            </div>
+            <button class="right__payment-btn">ОПЛАТИТЬ</button>
+        </div>
+    </div>
+    <hr class="mpa-hr">
+    <div class="mid-info">
+        <div class="mid-info__left">
+            <div class="left__serving">
+                <h3 class="serving__indication">Вас обслуживает:</h3>
+                <p class="serving__fillable">ПАО “Пермэнергосбыт”</p>
+            </div>
+            <div class="left__address">
+                <h3 class="address__indication">Адрес:</h3>
+                <p class="address__fillable">Пермский край 618250 г. Габуха, ул. Дегтярева д. 9</p>
+            </div>
+            <div class="left__telephone">
+                <h3 class="telephone__indication">телефоны:</h3>
+                <p class="telephone__fillable">8(342) 200-63-00, 8(342) 263-36-33</p>
+            </div>
+            <div class="left__working-hours">
+                <h3 class="working-hours__indication">Режим работы:</h3>
+                <p class="working-hours__fillable">пн-пт с 8-00 до 17-00, сб с 9-00 до 15-00, вс выходной</p>
+            </div>
+        </div>
+        <div class="mid-info__separator"></div>
+        <div class="mid-info__right">
+            <div class="right__invoice-indication">
+                <h3 class="invoice-indication__header">Доставка квитанций</h3>
+                <p class="invoice-indication__text">Подпишитесь на доставку электронных квитанций - наиболее<br>быстрый способ получения рсчетных документов</p>
+            </div>
+            <div class="right__buttons">
+                <div class="buttons__info">
+                    <p class="left-info__disabled">Доставка электронных<br>квитанций <span style="color: #FF0000; font-family: Gilroy-Bold, serif">отключена</span></p>
+                    <p class="left-info__activated">Доставка бумажных<br> квитанций <span style="color: #00822E; font-family: Gilroy-Bold, serif">включена</span></p>
+                </div>
+                <div class="buttons__buttons">
+                    <button class="buttons__btn-activate">Подпистаться</button>
+                    <button class="buttons__btn-deactivate">Отключить</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr class="mpa-hr">
+</main>
+
+<?php require_once("common/footer.php"); ?>
+<script src="js/main.js"></script>
+
+</body>
+</html>
